68ab9382366ecabda22cb8915091b65d5154933c
codeByScott <scott@codebyscott.io>
2017-09-02 23:19:54 +0900

    add member model with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root to: 'pages#home'
 
+  devise_for :members
+  
   get 'pages/about'
-
   get 'pages/privacy'
-
   get 'pages/terms'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
