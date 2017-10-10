3bce9d4cf6a2f207016fe36c6cc08136fedb1847
codeByScott <scott@codebyscott.io>
2017-10-10 16:32:56 +0900

    add basic templating

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   devise_for :members
-  
-  get 'pages/about'
-  get 'pages/privacy'
-  get 'pages/terms'
+
+  get '/about', to: 'pages#about', as: 'about'
+  get '/privacy', to: 'pages#privacy', as: 'privacy'
+  get '/terms', to: 'pages#terms', as: 'terms'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
