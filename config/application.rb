b91a34108cae349acbcdcf70ed29622720c94dbc
Amanda Dolan <amandacrawfordalc@gmail.com>
2017-11-08 09:53:14 -0500

    Don't automatically generate files that are only needed on occassion

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module Milspousecoders
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.view_specs false
+      g.jbuilder false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
